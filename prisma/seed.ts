7b37dd1ddbf6ffe649d34d7fc09e7e1fe61f52af
chudickgumanoid <sashaq0708@gmail.com>
2024-02-07 00:55:41 +0600

    feat🧹: add seed prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const measures = [
+    { name: "Measure 1", fullName: "Full Measure 1", code: 1 },
+    { name: "Measure 2", fullName: "Full Measure 2", code: 2 },
+  ];
+
+  await prisma.measure.createMany({
+    data: measures,
+  });
+
+  console.log("Seed completed successfully!");
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
